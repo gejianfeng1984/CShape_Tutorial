b4a435dc3b4456f2ba34c957077c52cdcaab6944
Ge Jian Feng <gejianfeng2@126.com>
2014-11-08 11:37:42 +0800

    + Add Sample Code

---
+++ CH02/CH02_01_HelloWorld/CH02_01_HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH02_01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The first app in Beginning C# Programming!");
+            Console.ReadKey();
+        }
+    }
+}
