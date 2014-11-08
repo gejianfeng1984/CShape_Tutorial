6b260933528c3c045f6db0969bbaca61e2ac7e80
Ge Jian Feng <gejianfeng2@126.com>
2014-11-08 20:41:46 +0800

    + Add Sample Code

---
+++ CH09/CH09_01_DefineClassAndInterface/CH09_01_DefineClassAndInterface/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH09_01_DefineClassAndInterface
+{
+    class Program
+    {
+        public abstract class MyBase
+        {
+
+        }
+
+        internal class MyClass : MyBase
+        {
+
+        }
+
+        public interface IMyBaseInterface
+        {
+
+        }
+
+        public interface IMyBaseInterface2
+        {
+
+        }
+
+        internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2
+        {
+
+        }
+
+        internal class MyComplexClass : MyClass, IMyInterface
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            MyComplexClass myObj = new MyComplexClass();
+            Console.WriteLine(myObj.ToString());
+            Console.ReadKey();
+        }
+    }
+}
