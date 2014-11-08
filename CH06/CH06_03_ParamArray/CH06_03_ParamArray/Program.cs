1a27751f0bb7dddd7f4f441ac1f574a000802ed2
Ge Jian Feng <gejianfeng2@126.com>
2014-11-08 17:12:39 +0800

    + Add Sample Code

---
+++ CH06/CH06_03_ParamArray/CH06_03_ParamArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH06_03_ParamArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = SumVals(1, 5, 2, 9, 8);
+            Console.WriteLine("Summed Values = {0}", sum);
+            Console.ReadKey();
+        }
+
+        static int SumVals(params int[] vals)
+        {
+            int sum = 0;
+            foreach (int val in vals)
+            {
+                sum += val;
+            }
+            return sum;
+        }
+    }
+}
