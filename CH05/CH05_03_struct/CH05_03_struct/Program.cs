c43c6448e9a3cfad320fb9d5d5b172dc1018f69b
Ge Jian Feng <gejianfeng2@126.com>
2014-11-08 15:46:40 +0800

    + Add Sample Code

---
+++ CH05/CH05_03_struct/CH05_03_struct/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH05_03_struct
+{
+    enum orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4
+    }
+
+    struct route
+    {
+        public orientation direction;
+        public double distance;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            route myRoute;
+            int myDirection = -1;
+            double myDistance;
+
+            Console.WriteLine("1) North\n2) South\n3) East\n4) West");
+
+            do
+            {
+                Console.WriteLine("Select a direction:");
+                myDirection = Convert.ToInt32(Console.ReadLine());
+            } while ((myDirection < 1) || (myDirection > 4));
+
+            Console.WriteLine("Input a distance:");
+            myDistance = Convert.ToDouble(Console.ReadLine());
+            
+            myRoute.direction = (orientation)myDirection;
+            myRoute.distance = myDistance;
+
+            Console.WriteLine("myRoute specifices a direction of {0} and a distance of {1}", myRoute.direction, myRoute.distance);
+            Console.ReadKey();
+        }
+    }
+}
