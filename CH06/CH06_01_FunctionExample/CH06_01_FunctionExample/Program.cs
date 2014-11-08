c564fff4811cb8bfe19d04f8bc918b77636b10f8
Ge Jian Feng <gejianfeng2@126.com>
2014-11-08 17:01:17 +0800

    + Add Sample Code

---
+++ CH06/CH06_01_FunctionExample/CH06_01_FunctionExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH06_01_FunctionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+            Console.ReadKey();
+        }
+
+        static void Write()
+        {
+            Console.WriteLine("Text output from function.");
+        }
+    }
+}
