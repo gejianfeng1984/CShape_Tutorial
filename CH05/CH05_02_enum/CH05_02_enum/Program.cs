5ba1e1df6b7c26a516047781334d26939762089e
Ge Jian Feng <gejianfeng2@126.com>
2014-11-08 15:38:20 +0800

    + Add Sample Code

---
+++ CH05/CH05_02_enum/CH05_02_enum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH05_02_enum
+{
+    enum orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            orientation myDirection = orientation.north;
+            Console.WriteLine("myDirection = {0}", myDirection);
+            Console.ReadKey();
+        }
+    }
+}
