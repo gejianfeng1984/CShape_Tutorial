200c8a4127fdf5e8f182b733ec1d9ad0f8458e6b
Ge Jian Feng <gejianfeng2@126.com>
2014-11-08 16:57:54 +0800

    + Add Sample Code

---
+++ CH05/CH05_05_String/CH05_05_String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH05_05_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "This is a test.";
+            char[] separator = { ' ' };
+            string[] myWords;
+            myWords = myString.Split(separator);
+            foreach (string word in myWords)
+            {
+                Console.WriteLine("{0}", word);
+            }
+            Console.ReadKey();
+        }
+    }
+}
