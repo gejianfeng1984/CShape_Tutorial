aebb68bcd42daa4ae08d34da7c0c4389208e0dea
Ge Jian Feng <gejianfeng2@126.com>
2014-11-08 20:50:56 +0800

    + Add Sample Code

---
+++ CH09/CH09_02_ClassLib/CH09_02_ClassLib/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLib;
+
+namespace CH09_02_ClassLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyExternalClass myObj = new MyExternalClass();
+            Console.WriteLine(myObj.ToString());
+            Console.ReadKey();
+        }
+    }
+}
