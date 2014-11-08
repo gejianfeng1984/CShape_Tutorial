bb7192aad10a2ba1a0c64ce39676997d260bbd0e
Ge Jian Feng <gejianfeng2@126.com>
2014-11-08 19:25:13 +0800

    + Add Sample Code

---
+++ CH07/CH07_02_Exception/CH07_02_Exception/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH07_02_Exception
+{
+    class Program
+    {
+        static string[] eTypes = { "none", "simple", "index", "nested index" };
+
+        static void Main(string[] args)
+        {
+            foreach (string eType in eTypes)
+            {
+                try
+                {
+                    Console.WriteLine("Main() try block reached.");
+                    Console.WriteLine("ThrowException(\"{0}\") called.", eType);
+                    ThrowException(eType);
+                    Console.WriteLine("Main() try block continue.");
+                }
+                catch (System.IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Main() System.IndexOutOfRangeException catch block reached. Message:\n\"{0}\"", e.Message);
+                }
+                catch
+                {
+                    Console.WriteLine("Main() general catch block reached.");
+                }
+                finally
+                {
+                    Console.WriteLine("Main() finally block reached.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ThrowException(string exceptionType)
+        {
+            Console.WriteLine("ThrowException(\"{0}\") reached.", exceptionType);
+            switch (exceptionType)
+            {
+                case "none":
+                    Console.WriteLine("Not throwing an exception.");
+                    break;
+                case "simple":
+                    Console.WriteLine("Throwing System.Exception.");
+                    throw (new System.Exception());
+                    break;
+                case "index":
+                    Console.WriteLine("Throwing System.IndexOutOfRangeException.");
+                    eTypes[4] = "error";
+                    break;
+                case "nested index":
+                    try
+                    {
+                        Console.WriteLine("ThrowException(\"nested index\") try block reached.");
+                        Console.WriteLine("ThrowException(\"index\") called.");
+                        ThrowException("index");
+                    }
+                    catch
+                    {
+                    }
+                    finally
+                    {
+                    }
+                    break;
+            }
+        }
+    }
+}
